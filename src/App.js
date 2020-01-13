d45278c5d6bc8d08995d1fdafc11ed427acf5f02
Shan-Yi <a24215188@gmail.com>
2020-01-13 16:16:27 +0800

    0113

--- src/App.js
+++ src/App.js
@@ -3,15 +3,6 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
   return (
     <>
       <h1>Hello World</h1>
